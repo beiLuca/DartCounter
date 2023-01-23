db31f0ce705ffe436243bf3e7c028827e2385054
beiLuca <109134544+beiLuca@users.noreply.github.com>
2023-01-23 11:52:10 +0100

    Add files via upload
    Initial Commit -> maven Projekt

---
+++ dartcounter/dartcounter/src/main/java/de/dart/dartcounter/DartcounterApplication.java
@@ -0,0 +1,13 @@
+package de.dart.dartcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartcounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DartcounterApplication.class, args);
+	}
+
+}
---
+++ dartcounter/dartcounter/src/test/java/de/dart/dartcounter/DartcounterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dart.dartcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartcounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
